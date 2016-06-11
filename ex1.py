10b530bcb45594d60985503496e31bf1979bc7d3
Jason_zheng <JasonLearning@icloud.com>
2016-06-11 16:10:06 +0800

    Day1, ex0+ex1; [1p]

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#just print; "" ;'' ; and #;
+#print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing!'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Hehe means 'well, feel free to go on, though I don't give a shit."
